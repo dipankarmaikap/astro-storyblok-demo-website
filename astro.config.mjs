873c0cdca020dfd7b121f31d6d0158b534de2464
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-06-24 18:59:25 +0530

    Tailwind and local ssl added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import mkcert from "vite-plugin-mkcert";
+import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+const { STORYBLOK_LOCAL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+const isLocal = STORYBLOK_LOCAL === "yes";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  ...(isLocal && {
+    vite: {
+      server: {
+        https: true,
+      },
+      plugins: [mkcert()],
+    },
+  }),
+});
